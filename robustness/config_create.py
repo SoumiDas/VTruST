b06d5f802108198f8de7c578b2e48d8bd6825367
SoumiDas <33958408+SoumiDas@users.noreply.github.com>
2023-05-23 11:52:45 +0530

    Add files via upload

--- robustness/config_create.py
+++ robustness/config_create.py
@@ -3,20 +3,16 @@ import json
 params = {}
 
 #Flags for Trajectory Selection
-params['csel'] = True #True for executing CheckSel during training
+params['csel'] = True #True for executing VTruST during training
 params['resume'] = False #True for resuming training where it was left off
 
 #Flags for Data Valuation
 params['retain_scores'] = False #False for new/replaced computation of data values, True for using existing ones
 
-#Flags for finding subset if required
-params['findsubset'] = True #Flag for obtaining subset
-params['simsel'] = False #Flag for using SubSel to obtain subset
-
-#Hyperparameters during Trajectory Selection
+#Hyperparameters during selection
 params['trainbatch'] = 100 #Training data batch size
 params['testbatch'] = 100 #Test data batch size
-params['epochs'] = 2 #Training epochs
+params['epochs'] = 100 #Training epochs
 params['num_freqep'] = 40 #Optional ; Frequency of epochs at which VTruST will be executed
 params['num_trajpoint'] = 1300 #Number of to-be selected batches
 
